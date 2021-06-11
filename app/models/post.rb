17d61db4df9714bb025e4170a1b201c09245f7b8
Jaskaran <er.jkjaskaran@gmail.com>
2021-06-11 23:03:53 +0530

    User and commments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+	belongs_to :user
 	validates :title,uniqueness:{case_sensitive: false}, presence: true, length: {minimum:1, maximum: 50}
 	validates :description, presence: true, length: {minimum:10, maximum:500}
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :users
   resources :friends,:posts
   get 'home/index'
   get 'users/sign_in'
