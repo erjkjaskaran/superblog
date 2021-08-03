b0c866d14f068bc6cf7d6077fe80bcd9735427c5
Kanwal Preet Singh <kpsingh0801@gmail.com>
2021-08-03 09:37:40 +0530

    heroku

--- app/mailers/ordermail_mailer.rb
+++ app/mailers/ordermail_mailer.rb
@@ -1,7 +1,6 @@
 class OrdermailMailer < ApplicationMailer
     def new_order_email
         @post=params[:post]
-        debugger
         mail(to: @post.description, subject: "Order Confirmed!")
     end
 end
