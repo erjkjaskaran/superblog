28d3d8d51589c0e2d0db56843a222873692aac24
Kanwal Preet Singh <kpsingh0801@gmail.com>
2021-08-03 15:33:45 +0530

    push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,7 +105,7 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address => 'smtp.sendgrid.net',
-    :port => '587'
+    :port => '587',
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :authentication => :plain,
