08128ed2da01e9c5c795a0731e846b9aeb160173
Kanwal Preet Singh <kpsingh0801@gmail.com>
2021-08-03 20:41:56 +0530

    gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -97,7 +97,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
   config.action_mailer.delivery_method = :smtp
   host = 'superblog.herokuapp.com' #replace with your own url
   config.action_mailer.default_url_options = { host: host, :protocol => 'https' }
@@ -105,13 +104,13 @@ Rails.application.configure do
   # SMTP settings for mailtrap
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name => '2d9671a663d5e0',
-    :password => 'bc52f395622849',
-    :address => 'smtp.mailtrap.io',
-    :domain => 'smtp.mailtrap.io',
-    :port => '2525',
-    :authentication => :cram_md5
-  }
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            Rails.application.credentials.dig(:gmail_smtp, :username),
+    password:             Rails.application.credentials.dig(:gmail_smtp, :password),
+    authentication:       'plain',
+    enable_starttls_auto: true }
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
